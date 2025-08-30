29279938999609f08de6d878bd78b64e36de2116
wizwiz <abezzi@outlook.cl>
2025-08-30 18:13:26 -0400

    chore(style): removed unused imports

--- app/categories/new-category.tsx
+++ app/categories/new-category.tsx
@@ -1,7 +1,3 @@
-import { View } from "react-native";
-import { Text } from "~/components/ui/text";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import { CategoryForm } from "~/components/category/category-form";
 
 export default function NewCategory() {
